eaee21a5bfc24f62b628ce64466fa0b28dfd22a9
arjun.chamyal@zeeve.io <Arjun Chamyal>
2022-02-10 02:59:52 +0530

    Delta added on Thu Feb 10 02:59:52 IST 2022 from prima-materia by abhi

---
+++ examples/bench-server.rs
@@ -0,0 +1,16 @@
+/// WebSocket server used for testing the bench example.
+extern crate axia_ws as ws;
+
+use ws::{Builder, Sender, Settings};
+
+fn main() {
+    let mut settings = Settings::default();
+    settings.max_connections = 10_000;
+
+    Builder::new()
+        .with_settings(settings)
+        .build(|out: Sender| move |msg| out.send(msg))
+        .unwrap()
+        .listen("127.0.0.1:3012")
+        .unwrap();
+}
